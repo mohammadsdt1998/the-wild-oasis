4720b4f22b7fe15efb3c5aad316ab3437f9a6570
mohammad sadati <m.sadati76@gmail.com>
2024-07-15 18:18:07 +0330

    set up pages and router

--- src/pages/Account.jsx
+++ src/pages/Account.jsx
@@ -1,9 +1,10 @@
+import React from "react";
 import Heading from "../ui/Heading";
 import Row from "../ui/Row";
 
 function Account() {
   return (
-    <>
+    <React.Fragment>
       <Heading as="h1">Update your account</Heading>
 
       <Row>
@@ -15,7 +16,7 @@ function Account() {
         <Heading as="h3">Update password</Heading>
         <p>Update user password form</p>
       </Row>
-    </>
+    </React.Fragment>
   );
 }
 
