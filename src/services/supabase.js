675f595034a8e0347e0c070b203313883e60dc5d
mohammad sadati <m.sadati76@gmail.com>
2024-07-19 16:32:07 +0330

    create cabin with useForm hook

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,5 +1,5 @@
 import { createClient } from "@supabase/supabase-js";
-const supabaseUrl = "https://andntmcyuubdvgbzzqsy.supabase.co";
+export const supabaseUrl = "https://andntmcyuubdvgbzzqsy.supabase.co";
 const supabaseKey =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFuZG50bWN5dXViZHZnYnp6cXN5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjA2MjI2MjEsImV4cCI6MjAzNjE5ODYyMX0.EgjzU7C-ksmTkmwwfAAeKOH9Kxe0d35KDIVUE0BEYec";
 const supabase = createClient(supabaseUrl, supabaseKey);
