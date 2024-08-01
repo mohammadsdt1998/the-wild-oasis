230ba4ac71154d80b2ecda4cd0575e6e7517c137
mohammad sadati <m.sadati76@gmail.com>
2024-08-01 21:41:23 +0330

    sort bookings in server side

--- src/features/bookings/useBookings.js
+++ src/features/bookings/useBookings.js
@@ -5,19 +5,28 @@ import { useSearchParams } from "react-router-dom";
 export function useBookings() {
   const [searchParams] = useSearchParams();
 
+  // FILTER
+
   const filterValue = searchParams.get("status");
   const filter =
     !filterValue || filterValue === "all"
       ? null
       : { field: "status", value: filterValue };
 
+  // SORT
+
+  const sortByRaw = searchParams.get("sortBy") || "startDate-desc";
+
+  const [field, direction] = sortByRaw.split("-");
+  const sortBy = { field, direction };
+
   const {
     isLoading,
     data: bookings,
     error,
   } = useQuery({
-    queryKey: ["bookigns", filter],
-    queryFn: () => getBookings({ filter }),
+    queryKey: ["bookigns", filter, sortBy],
+    queryFn: () => getBookings({ filter, sortBy }),
   });
 
   return { isLoading, bookings, error };
