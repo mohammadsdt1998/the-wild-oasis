e8a539ec333c644b259066785305d52ccfd33db3
mohammad sadati <m.sadati76@gmail.com>
2024-08-10 19:41:49 +0330

    create protected route for authorization

--- src/features/authentication/useLogin.js
+++ src/features/authentication/useLogin.js
@@ -1,15 +1,17 @@
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { login as loginApi } from "../../services/apiAuth";
 import toast from "react-hot-toast";
 import { useNavigate } from "react-router-dom";
 
 export function useLogin() {
+  const queryClient = useQueryClient();
   const navigate = useNavigate();
   const { mutate: login, isLoading } = useMutation({
     mutationFn: ({ email, password }) => loginApi({ email, password }),
-    onSuccess: () => {
+    onSuccess: (user) => {
       toast.success("login successfully");
-      navigate("/");
+      queryClient.setQueriesData(["user"], user);
+      navigate("/dashboard ");
     },
     onError: () => toast.error("Provided email or password are incorrect"),
   });
