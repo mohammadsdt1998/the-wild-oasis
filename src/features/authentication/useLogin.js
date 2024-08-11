6c4d6f86b545c13fbe30f1d883cde67b1aa6f511
mohammad sadati <m.sadati76@gmail.com>
2024-08-11 23:27:49 +0330

    debug useLogin

--- src/features/authentication/useLogin.js
+++ src/features/authentication/useLogin.js
@@ -10,7 +10,7 @@ export function useLogin() {
     mutationFn: ({ email, password }) => loginApi({ email, password }),
     onSuccess: (user) => {
       toast.success("login successfully");
-      queryClient.setQueriesData(["user"], user);
+      queryClient.setQueryData(["user"], user.user);
       navigate("/dashboard", { replace: true });
     },
     onError: () => toast.error("Provided email or password are incorrect"),
