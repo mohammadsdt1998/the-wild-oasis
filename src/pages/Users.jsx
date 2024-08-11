f5faf5de9465e99c0cb4465170f16e6685aad33a
mohammad sadati <m.sadati76@gmail.com>
2024-08-11 23:55:03 +0330

    create signup form and validate inputs

--- src/pages/Users.jsx
+++ src/pages/Users.jsx
@@ -1,7 +1,14 @@
+import React from "react";
+import SignupForm from "../features/authentication/SignupForm";
 import Heading from "../ui/Heading";
 
 function Users() {
-  return <Heading as="h1">Create a new user</Heading>;
+  return (
+    <React.Fragment>
+      <Heading as="h1">Create a new user</Heading>
+      <SignupForm />
+    </React.Fragment>
+  );
 }
 
 export default Users;
