1e0596e4361464588839ec9eb184d16936c5a207
mohammad sadati <m.sadati76@gmail.com>
2024-08-14 21:48:14 +0330

    create header menu

---
+++ src/ui/HeaderMenu.jsx
@@ -0,0 +1,30 @@
+import styled from "styled-components";
+import ButtonIcon from "./ButtonIcon";
+import { HiOutlineUser } from "react-icons/hi";
+import { useNavigate } from "react-router-dom";
+import Logout from "../features/authentication/Logout";
+import UserAvatar from "../features/authentication/UserAvatar";
+
+const StyledHeaderMenu = styled.ul`
+  display: flex;
+  gap: 0.4 rem;
+`;
+
+function HeaderMenu() {
+  const navigate = useNavigate();
+  return (
+    <StyledHeaderMenu>
+      <li>
+        <ButtonIcon onClick={() => navigate("/account")}>
+          <HiOutlineUser />
+        </ButtonIcon>
+      </li>
+
+      <li>
+        <Logout />
+      </li>
+    </StyledHeaderMenu>
+  );
+}
+
+export default HeaderMenu;
